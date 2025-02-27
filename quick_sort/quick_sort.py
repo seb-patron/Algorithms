3797188eb5f4b1b2b7439c550e090358ec44451f
Sebastian Patron-Soto <sebastian.patrones@gmail.com>
2025-02-26 18:35:29 -0600

    Improve quick_sort by using random pivot selection for better average case performance

--- quick_sort/quick_sort.py
+++ quick_sort/quick_sort.py
@@ -1,11 +1,13 @@
-
+import random
 
 def quick_sort(arr):
     if len(arr) < 2:
         return arr
     else:
-        pivot = arr[0]
-        less = [i for i in arr[1:] if i <= pivot]
-        greater = [i for i in arr[1:] if i > pivot]
+        pivot_index = random.randint(0, len(arr) - 1)
+        pivot = arr[pivot_index]
+        
+        less = [i for i in arr[:pivot_index] + arr[pivot_index + 1:] if i <= pivot]
+        greater = [i for i in arr[:pivot_index] + arr[pivot_index + 1:] if i > pivot]
 
         return quick_sort(less) + [pivot] + quick_sort(greater)
